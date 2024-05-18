8740789c965f9584b560b34a4f0ccfe16686b96b
Prerana Sapkota <86253756+prerana30@users.noreply.github.com>
2024-05-18 17:14:35 +0000

    Learned about string and mutations

--- Start/Ch1_Intro/Basic/Program.cs
+++ Start/Ch1_Intro/Basic/Program.cs
@@ -1,11 +1,20 @@
 ﻿// Example file for C# Applied Data Structures by Joe Marini
 // Basic data structures
 
+string s1 = "Hello world";
+char[] c1 = new char[] { 'f', 'a', 'm', 'i', 'l', 'y' };
 // TODO: strings are collections of characters, and are immutable
+Console.WriteLine(s1);
+char[] c2 = new char[] { 'F', 'i', 'z', 'z', 'y', ' ', 'B', 'u', 'z', 'z' };
+string s2 = s1;
 
-// char[] c1 = new char[] {'F','i','z','z','y',' ','B','u','z','z'};
-
-
+string s3 = new string(c2);
+Console.WriteLine(s3);
+Console.WriteLine(s2);
+Console.WriteLine(s1);
+char[] c5= {'a', 'b'};
+string s4 = new string(c5);
+Console.WriteLine(c5);
 // TODO: Be careful with string references
 
 
